c93b7eaff37d490eafb792b69acf9bf4e9c07b9a
Vittorio <vikrisjb@gmail.com>
2024-05-08 18:23:02 +0200

    time string fix

--- webapp/controller/Mainview.controller.js
+++ webapp/controller/Mainview.controller.js
@@ -832,16 +832,24 @@ sap.ui.define([
             _msToHMS: function (ms) {
                 // 1- Convert to seconds:
                 var seconds = ms / 1000;
+                
                 // 2- Extract hours:
                 var hours = parseInt(seconds / 3600); // 3,600 seconds in 1 hour
                 if (hours.toString().length === 1) {
                     hours = "0" + hours;
                 }
                 seconds = seconds % 3600; // seconds remaining after extracting hours
+                
                 // 3- Extract minutes:
                 var minutes = parseInt(seconds / 60); // 60 seconds in 1 minute
+                if (minutes.toString().length === 1) {
+                    minutes = "0" + minutes;
+                }
                 // 4- Keep only seconds not extracted to minutes:
                 seconds = seconds % 60;
+                if (seconds.toString().length === 1) {
+                    seconds = "0" + seconds;
+                }
                 var finalTime = (hours + ":" + minutes + ":" + seconds);
                 return finalTime;
             },
